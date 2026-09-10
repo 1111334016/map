bca9c6e52f0308a0400297144ebe3da615adef21
1111334016 <liangsu168888@gmail.com>
2026-09-10 11:44:43 +0800

    Update GAS_WEB_APP_URL to new endpoint

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', async () => {
-  const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbybPsM6jjhXRMFl0rZ8ntctPtqP1mJz2LXk8CufoQWO5lpjMHMiDjLT7n5DFnvwhjvVxQ/exec';
+  const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzvZNkCy0Q-jhgo7yzgMCAjr8ap2ez4R8ZuxFJDYpGuN7Fzghc3ByrnrxAtG6eewnTSnw/exec';
   const MY_LIFF_ID = '2011288416-9QX9xXhz'; // 請確認此 ID 與 LINE Developers 設定一致
 
   let currentUserId = '';
